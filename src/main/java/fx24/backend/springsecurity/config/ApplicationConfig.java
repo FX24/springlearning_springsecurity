61eed28c067624aa04cc1499fd7c16b4ae136c14
FX24 <muhammad.farhan.fm00@gmail.com>
2023-02-04 03:09:53 +0700

    Implement UserDetailService, validate and update the SecurityContext

---
+++ src/main/java/fx24/backend/springsecurity/config/ApplicationConfig.java
@@ -0,0 +1,21 @@
+package fx24.backend.springsecurity.config;
+
+import fx24.backend.springsecurity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+    }
+}
--- src/main/java/fx24/backend/springsecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/fx24/backend/springsecurity/config/JwtAuthenticationFilter.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,12 +19,41 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
 
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {          //We have user email and not authenticated
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);                //Get user details from DB
+            if (jwtService.isTokenValid(jwt, userDetails)) {                                                //Check if the token valid or not
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(    //if valid -> create authToken
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authToken.setDetails(                                                                       //Extend the authToken with details of the request
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);                            //Update authToken
+            }
+        }
+        filterChain.doFilter(request, response);                                                            //Always pass filterChain so the next filter to be executed
     }
 }
